31e6363ea024e7561e6a6cf1812b969eec89b974
Jeremy Silver <jeremys@nessiness.com>
2023-01-23 16:26:40 -0500

    Made graphs have integer nodes; various fixes to algorithms.

--- git_split.py
+++ git_split.py
@@ -4,7 +4,7 @@ from functools import cache, cached_property
 import networkx as nx
 import os
 import re
-from typing import Any, List, Optional, Set
+from typing import Any, Dict, List, Optional, Set
 
 from git import Blob, Commit, Repo, Tree
 
@@ -22,6 +22,11 @@ def get_import_from_line(line: str) -> Optional[str]:
                 return s
     return None
 
+def path_relative_to(path: str, relative_to: str) -> str:
+    if path.startswith(relative_to):
+        return path.removeprefix(relative_to).lstrip('/')
+    raise ValueError(f'{path} is not a subpath of {relative_to}')
+
 @dataclass
 class PathData:
     path: str
@@ -30,42 +35,23 @@ class PathData:
 def path_data_from_tree(tree: Tree) -> List[PathData]:
     return [PathData(obj.abspath, isinstance(obj, Tree)) for obj in tree.traverse()]
 
-@cache
-def files_edited_by_commit(commit: Commit) -> List[str]:
-    paths = []
-    for path in commit.stats.files:
-        match = RENAME_REGEX.match(path)
-        if match:  # file was renamed
-            groups = match.groups()
-            ps = [groups[0] + groups[1], groups[0] + groups[2]]
-        else:
-            ps = [path]
-        for p in ps:
-            if (not os.path.isabs(p)):
-                p = os.path.join(commit.repo.working_tree_dir, p)
-            paths.append(p)
-    return paths
-
 def get_blob_contents(blob: Blob) -> bytes:
     return blob.data_stream[-1].read()
 
 def module_refers_to_path(mod_name: str, path: str) -> bool:
     segs1 = [seg for seg in mod_name.split('.') if seg]
-    segs2 = [seg for seg in path.removesuffix('.py').split('/') if seg]
-    return segs2[-len(segs1):] == segs1
+    segs2 = [seg for seg in path.removesuffix('__init__.py').removesuffix('.py').split('/') if seg]
+    return segs1[:len(segs2)] == segs2
 
 def get_best_ordered_partition(dg: nx.DiGraph) -> Partition:
     def _get_subpartition(subgraph: nx.DiGraph) -> Partition:
         # assume nodes are in a canonical ordering
         if (len(subgraph) == 0):
             return []
-        # get the first parent of u (if it exists)
+        assert sorted(list(subgraph)) == list(subgraph)
         n = subgraph.number_of_nodes()
         u = next(iter(subgraph))
-        try:
-            parent = next(subgraph.predecessors(u))
-        except StopIteration:
-            parent = None
+        parents = set(subgraph.predecessors(u))
         subgraph = subgraph.copy()
         # remove u and compute connected components
         subgraph.remove_node(u)
@@ -76,13 +62,15 @@ def get_best_ordered_partition(dg: nx.DiGraph) -> Partition:
             comp = subgraph.subgraph(comp_set)
             subpartition += _get_subpartition(comp)
         # determine where to put u
-        if (parent is None):  # u gets its own component
-            subpartition.insert(0, [u])
-        else:  # u joins the component of its parent
-            for part in subpartition:
-                if (parent in part):
+        if parents:
+            for (i, part) in enumerate(subpartition):
+                if parents.intersection(part):  # u joins the first component with a parent
                     part.insert(0, u)
+                    break
+        else:  # u gets its own component
+            subpartition.insert(0, [u])
         assert (sum(map(len, subpartition)) == n)
+        assert all(sorted(part) == part for part in subpartition)
         return subpartition
     return _get_subpartition(dg)
 
@@ -112,11 +100,31 @@ class GitSplitter:
             if (commit == self.first_commit):
                 break
         return hist[::-1]
+    @property
+    def repo_dir(self) -> str:
+        assert (self.repo.working_tree_dir is not None)
+        return str(self.repo.working_tree_dir)
+    @cache
+    def files_edited_by_commit(self, commit: Commit) -> List[str]:
+        paths = []
+        for path in commit.stats.files:
+            match = RENAME_REGEX.match(path)
+            if match:  # file was renamed
+                groups = match.groups()
+                ps = [groups[0] + groups[1], groups[0] + groups[2]]
+            else:
+                ps = [path]
+            for p in ps:
+                if os.path.isabs(p):
+                    p = path_relative_to(p, self.repo_dir)
+                paths.append(p)
+        return paths
     @cached_property
     def all_file_paths(self) -> List[str]:
         all_paths: Set[str] = set()
+        workdir = str(self.repo.working_tree_dir)
         for commit in self.history:
-            all_paths.update([pd.path for pd in path_data_from_tree(commit.tree) if (not pd.is_dir)])
+            all_paths.update([path_relative_to(pd.path, workdir) for pd in path_data_from_tree(commit.tree) if (not pd.is_dir)])
         return sorted(all_paths)
     @cached_property
     def all_python_file_paths(self) -> List[str]:
@@ -124,10 +132,9 @@ class GitSplitter:
     def python_dependencies(self, commit: Commit, path: str) -> List[str]:
         """For a given commit and file path, gets the set of files on which it depends (in the commit's tree)."""
         if path.endswith('.py'):
-            relpath = os.path.relpath(path, self.repo.working_tree_dir)
             dependencies = set()
             try:
-                blob = commit.tree[relpath]
+                blob = commit.tree[path]
             except KeyError:
                 return []
             for line in get_blob_contents(blob).split(b'\n'):
@@ -141,26 +148,26 @@ class GitSplitter:
         return []
     @cached_property
     def commit_file_graph(self) -> nx.Graph:
-        """Gets a bipartite graph between commits and files."""
+        """Gets a bipartite graph between commit indices and files."""
         g = nx.Graph()
-        for commit in self.history:
-            for path in files_edited_by_commit(commit):
-                g.add_edge(commit, path)
+        for (i, commit) in enumerate(self.history):
+            for path in self.files_edited_by_commit(commit):
+                g.add_edge(i, path)
         return g
     @cached_property
     def commit_file_dependency_graph(self) -> nx.DiGraph:
         """Dependency graph between (commit, file) pairs.
         A pair (C2, F2) depends on a pair (C1, F1) if F1 is a Python dependency of F2 (in C2), and C1 is an ancestor of C2."""
         dg = nx.DiGraph()
-        commits_by_path = defaultdict(list)
-        for commit in self.history:
-            for path in files_edited_by_commit(commit):
-                dg.add_node((commit, path))
+        commit_idx_by_path: Dict[str, List[int]] = defaultdict(list)
+        for (i, commit) in enumerate(self.history):
+            for path in self.files_edited_by_commit(commit):
+                dg.add_node((i, path))
                 deps = self.python_dependencies(commit, path)
                 for dep in deps:
-                    for ancestor in commits_by_path[dep]:
-                        dg.add_edge((commit, path), (ancestor, dep))
-                commits_by_path[path].append(commit)
+                    for j in commit_idx_by_path[dep]:
+                        dg.add_edge((i, path), (j, dep))
+                commit_idx_by_path[path].append(i)
         return dg
     @cached_property
     def commit_dependency_graph(self) -> nx.DiGraph:
@@ -168,17 +175,17 @@ class GitSplitter:
         Commit C2 depends on commit C1 (represented by an arrow from C2 to C1) if at least one file in C2 depends (in C2) on a file in C1."""
         dg1 = self.commit_file_dependency_graph
         dg2 = nx.DiGraph()
-        nodes_by_commit = defaultdict(list)
-        for (commit, path) in dg1:
-            nodes_by_commit[commit].append((commit, path))
-            dg2.add_node((commit, path))
-        for commit in self.history:
-            nodes = nodes_by_commit[commit]
-            for (c2, p2) in nodes:
-                for (c1, p1) in dg1[(c2, p2)]:
-                    # c1 must be an ancestor of c2; p2 must depend on p1 (in c2)
-                    if (c2 != c1):
-                        dg2.add_edge(c2, c1)
+        paths_by_commit_idx = defaultdict(list)
+        for (i, path) in dg1:
+            paths_by_commit_idx[i].append(path)
+            dg2.add_node(i)
+        for (j, _) in enumerate(self.history):
+            paths = paths_by_commit_idx[j]
+            for p2 in paths:
+                for (i, p1) in dg1[(j, p2)]:
+                    # i must be an ancestor of j; p2 must depend on p1 (in commit j)
+                    if (i != j):
+                        dg2.add_edge(j, i)
         return dg2
     def best_commit_partition(self) -> Partition:
         return get_best_ordered_partition(self.commit_dependency_graph)
